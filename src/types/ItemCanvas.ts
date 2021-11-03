737db00d897c7a19d874e138abca036ad5b749f9
Clio Lang <54592031+cliolang@users.noreply.github.com>
2021-11-03 10:46:54 -0400

    #9

--- src/types/ItemCanvas.ts
+++ src/types/ItemCanvas.ts
@@ -1,4 +1,5 @@
 import Item from "./Item";
+import ItemAnnotationPage from "./ItemAnnotationPage";
 
 /**
  * This is the type for the IIIF type "Canvas".
@@ -7,6 +8,7 @@ export default class ItemCanvas extends Item {
   readonly type: string = "Canvas";
   readonly width: number;
   readonly height: number;
+  annotationPages: ItemAnnotationPage[];
 
   /**
    * Create a new instance of Canvas
@@ -19,6 +21,43 @@ export default class ItemCanvas extends Item {
     super(id, undefined);
     this.width = width;
     this.height = height;
+  }
+
+  /**
+   * Add an ItemAnnotationPage to annotationPages
+   * 
+   * @param annotationPage the ItemAnnotationPage
+   */
+  addAnnotationPage(annotationPage: ItemAnnotationPage) {
+    try {
+      this.annotationPages.push(annotationPage);
+    } catch(e: unknown) {
+      console.log("Unable to add annotation page");
+    }
+    
+  }
+
+  /**
+   * Removes an ItemAnnotationPage to annotationPages
+   * 
+   * @param annotationPage the ItemAnnotationPage
+   */
+  removeAnnotationPage(annotationPage: ItemAnnotationPage) {
+    this.annotationPages.forEach((element, index)=> {
+      if(element == annotationPage) 
+        delete this.annotationPages[index];
+      else
+        console.log("Unable to find annotationPage to delete")
+    });
+  }
+
+  /**
+   * Returns AnnotationPages array
+   * 
+   * @returns annotationPages
+   */
+  getAnnotationPages() {
+    return this.annotationPages;
   }
 
   toJSONString(): string {
