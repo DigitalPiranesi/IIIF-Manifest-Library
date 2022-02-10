6cc3a7aa27b3173c624596cca8dfdbbd87eddab6
Clio Lang <54592031+cliolang@users.noreply.github.com>
2022-02-10 10:14:01 -0500

    suggestion notes for future meeting

--- json_of_book/BookInfoRetriever.js
+++ json_of_book/BookInfoRetriever.js
@@ -405,6 +405,14 @@ class RDFDecoder {
   var decoder = new RDFDecoder(config);
   var arrays = decoder.decode();
 
+
+  //Clio notes: Making this less 'code-manual' and more user-input like?
+  //Put in URL and name of page/image, and the rest of the information is similar
+  //EX: user inputs: http://piranesi-test.reclaim.hosting/mirador/media/Pantheon.json?iiif-manifest=1
+  // method parses 'Pantheon' as the main feature of the URL, inputs it into the '/____/' between media/ and /page or whatever
+  // endpoint that the URL requires
+  //Context will always be the same
+
   // Create Manifest
   var manifest = new I3.Manifest(3, "http://piranesi-test.reclaim.hosting/mirador/media/Pantheon.json?iiif-manifest=1");
   var canvas = new I3.ItemCanvas("http://piranesi-test.reclaim.hosting/mirador/media/pantheon/canvas/p1", 17711, 12932);
@@ -415,6 +423,9 @@ class RDFDecoder {
   var webanno = new I3.ItemWebAnnotationImage("https://piranesi-test.reclaim.hosting/walts-test-book/media/pantheon/annotation/p0001-image", "painting", canvas, "https://env-4072537.us.reclaim.cloud/iiif/2/pantheon.jpg/full/full/0/default.jpg", 17711, 12932);
   webanno.addContext("http://iiif.io/api/presentation/3/context.json");
 
+  //Clio notes: Make into separate method to pass in fields? From here through the annotations
+  //label, webannopage, canvas, manifest, canvas
+  //if variables != null then create whole manifest
   manifest.addLabel(label);
   webannopage.addItem(webanno);
   canvas.addItem(webannopage);
